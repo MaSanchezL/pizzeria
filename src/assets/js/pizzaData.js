1a7c32900e8f80fd4c397b1d6da14a2dd6091b9b
Miguel Sánchez <m_alberto89@hotmail.com>
2024-12-05 22:32:41 -0300

    update

---
+++ src/assets/js/pizzaData.js
@@ -0,0 +1,39 @@
+export const pizzas = [
+    {
+      img: "./images/margarita.jpeg",
+      nombre: "Margarita",
+      ingredientes: "Salsa, Mozzarella, Albahaca",
+      precio: "5.950"
+    },
+    {
+      img: "./images/pepperoni.jpeg",
+      nombre: "Pepperoni",
+      ingredientes: "Salsa, Mozzarella, Pepperoni",
+      precio: "6.950"
+    },
+    {
+      img: "./images/estaciones.jpeg",
+      nombre: "Quattro Stazioni",
+      ingredientes: "Salsa, Alcachofa, Champiñones, Aceitunas Negras, Ajo",
+      precio: "9.950"
+    },
+    {
+      img: "./images/marinara.jpeg",
+      nombre: "Marinara",
+      ingredientes: "Salsa, Ajo, Albahaca",
+      precio: "5.950"
+    },
+    {
+      img: "./images/quesos.jpeg",
+      nombre: "Quattro formaggi",
+      ingredientes: "Salsa, Mozzarella, Gorgonzola, Fontina, Parmesano",
+      precio: "10.950"
+    },
+    {
+      img: "./images/burrata.jpeg",
+      nombre: "Burrata",
+      ingredientes: "Salsa, Mozzarella, Albahaca, Burrata",
+      precio: "7.950"
+    }
+  ];
+  
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,65 +1,28 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import CardPizza from "./CardPizza";
+import { pizzas } from "../assets/js/pizzaData";
 
 const Home = () => {
   return (
-    <>
-     <Container className="py-4">
-      <Row className="justify-content-center">
-        <Col>
-          <CardPizza
-            img="./images/margarita.jpeg"
-            nombre="Margarita"
-            ingredientes="Salsa, Mozzarella, Albahaca"
-            precio="5.950"
-          ></CardPizza>
-        </Col>
-        <Col>
-          <CardPizza
-            img="./images/pepperoni.jpeg"
-            nombre="Pepperoni"
-            ingredientes="Salsa, Mozzarella, Pepperoni"
-            precio="6.950"
-          ></CardPizza>
-        </Col>
-        <Col>
-          <CardPizza
-            img="./images/estaciones.jpeg"
-            nombre="Quattro Stazioni"
-            ingredientes="Salsa, Alcachofa, Champiñones, Aceitunas Negras, Ajo"
-            precio="9.950"
-          ></CardPizza>
-        </Col>
-      </Row>
-      <Row className="justify-content-center">
-        <Col>
-          <CardPizza
-            img="./images/marinara.jpeg"
-            nombre="Marinara"
-            ingredientes="Salsa, Ajo, Albahaca"
-            precio="5.950"
-          ></CardPizza>
-        </Col>
-        <Col>
-          <CardPizza
-            img="./images/quesos.jpeg"
-            nombre="Quattro formaggi"
-            ingredientes="Salsa, Mozzarella, Gorgonzola, Fontina, Parmesano"
-            precio="1.0950"
-          ></CardPizza>
-        </Col>
-        <Col>
-          <CardPizza
-            img="./images/burrata.jpeg"
-            nombre="Burrata"
-            ingredientes="Salsa, Mozzarella, Albahaca, Burrata"
-            precio="7.950"
-          ></CardPizza>
-        </Col>
-      </Row>
-      </Container>
-    </>
+    <Container className="py-4">
+      {Array.from({ length: Math.ceil(pizzas.length / 3) }).map((_, rowIndex) => (
+        <Row className="justify-content-center" key={rowIndex}>
+          {pizzas
+            .slice(rowIndex * 3, rowIndex * 3 + 3)
+            .map((pizza, i) => (
+              <Col key={i}>
+                <CardPizza
+                  img={pizza.img}
+                  nombre={pizza.nombre}
+                  ingredientes={pizza.ingredientes}
+                  precio={pizza.precio}
+                />
+              </Col>
+            ))}
+        </Row>
+      ))}
+    </Container>
   );
 };
 
